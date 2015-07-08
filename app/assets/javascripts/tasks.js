9518f6d5d95d132c510056f8104ec0526f8f0979
antonpot <markoapotocnik@hotmail.com>
2015-07-08 22:01:23 +0200

    Comments added to tasks.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,9 +1,9 @@
-// var myTaskObject;
-
-// var currentPositionInTime;
 var numOfAllTaskEvents = 9;
 
+// Global variable not declared at beginning, as information is kept when task is changed
 myTaskObject = new Task('My Task', numOfAllTaskEvents);
 
+// pageload() is called when refreshed
 $(document).ready(pageLoad);
+// pageload() is called if acting like single page app (Turbolinks gem)
 $(document).on('page:load', pageLoad);
