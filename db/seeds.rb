140769d94fb4ed9c3d4673a32a8ba0e1ea6b4398
antonpot <markoapotocnik@hotmail.com>
2015-07-08 20:59:33 +0200

    Comments added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-20.times do
+num_of_tasks = 20
+
+# Create a set amount of fake tasks
+num_of_tasks.times do
   Task.create(
     owner: Faker::Name.name,
     deadline: Faker::Date.forward(50),
     completed: [true, false].sample,
     description: Faker::Hacker.say_something_smart
   )
-  sleep(0.1)
+  sleep(0.1) # Sleep is added so timestamps differ
 end
 
+# For each task create from 5 to 25 stages task was in
 Task.all.each do |task|
   (rand(20)+5).times do
     task.statuses.create(
