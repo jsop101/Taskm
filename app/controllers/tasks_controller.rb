b22eee4b1506e037801167adec6831ba5e733b6e
antonpot <markoapotocnik@hotmail.com>
2015-07-08 20:55:08 +0200

    Cleaned unused code and added comments

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,19 @@
 class TasksController < ApplicationController
+  # so that #respond_with can be used (gem 'responders' is needed)
   respond_to :html, :json
 
   def index
     @all_tasks = Task.all
-    p '#'*100
-    p params
   end
 
   def show
     @task = Task.find(params[:id])
-    p "?"*100
     if params["position_in_time"]
       @status = @task.statuses[params["position_in_time"].to_i]
-      # respond_to do |format|
-      # #   format.html { redirect_to @task}
-      #   format.json { render json: @status }
-      # end
       respond_with( @status )
     else
       @all_task_events = @task.statuses.length.to_s
-      respond_with(@task)#{"num_of_all_task_events" => @all_task_events})
-      # respond_to do |format|
-      #   format.json { render json: {"num_of_all_task_events" => @all_task_events} }
-      # end
+      respond_with(@task)
     end
 
   end
